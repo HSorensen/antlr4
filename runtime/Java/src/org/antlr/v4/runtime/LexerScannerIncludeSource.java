037669d73d1eaa15ed2038494b3838ce80d6ddcb
nttdatahenriksorensen <henrik.sorensen@nttdata.com>
2015-08-20 21:48:58 -0700

    Add comments

--- runtime/Java/src/org/antlr/v4/runtime/LexerScannerIncludeSource.java
+++ runtime/Java/src/org/antlr/v4/runtime/LexerScannerIncludeSource.java
@@ -1,7 +1,16 @@
 package org.antlr.v4.runtime;
 
+
+/**
+ * Interface that allow the lexer to include another file
+ * into the scanning stream.
+ */
 public interface LexerScannerIncludeSource {
 	
+	/**
+	 * The embedSource method reads the fileName and optionally substitutes text
+	 * before returning the CharStream for the file.
+	 */
 	public CharStream embedSource(String fileName, String substituteFrom, String substituteTo);
 	
 
--- runtime/Java/src/org/antlr/v4/runtime/LexerScannerIncludeSourceImpl.java
+++ runtime/Java/src/org/antlr/v4/runtime/LexerScannerIncludeSourceImpl.java
@@ -2,24 +2,33 @@ package org.antlr.v4.runtime;
 
 import java.io.IOException;
 
+/**
+ * Implementation of interface that allow the lexer to include another file
+ * into the scanning stream.
+ * The lexer expects a CharStream object to be returned.
+ */
 public class LexerScannerIncludeSourceImpl implements LexerScannerIncludeSource {
-
-	@Override
-	public CharStream embedSource(String fileName, String substituteFrom,String substituteTo) {
-		ANTLRInputStream istrm=null;
+	
+	/**
+	 * The embedSource method reads the fileName and optionally substitutes text
+	 * before returning the CharStream for the file.
+	 */
+	public CharStream embedSource(String fileName, String substituteFrom, String substituteTo) {
+		ANTLRInputStream istrm = null;
 		try {
 			istrm = new ANTLRFileStream(fileName);
 			if (substituteFrom != null) {
-				String beforeStream = String.copyValueOf(istrm.data, 0,istrm.size());
-				String replacedStream = beforeStream.replaceAll(substituteFrom,substituteTo);
+				String beforeStream = String.copyValueOf(istrm.data, 0,
+						istrm.size());
+				String replacedStream = beforeStream.replaceAll(substituteFrom,
+						substituteTo);
 				istrm = new ANTLRInputStream(replacedStream);
 			}
 		} catch (IOException e) {
-			//TODO: Add error handling
+			// TODO: Add error handling
 			e.printStackTrace();
 		}
-		
+
 		return istrm;
 	}
-
 }
